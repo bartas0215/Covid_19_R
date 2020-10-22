b7039231b304632b1a90195f401ff11776240d0b
bartas0215 <poz.bartosznowak@gmail.com>
2020-10-22 22:49:52 +0200

    Addition of interactive table to shiny app

--- shiny_covid.R
+++ shiny_covid.R
@@ -1,15 +1,18 @@
 install.packages("shiny")
+install.packages("reactable")
 library(shiny)
 library(tidyverse)
-
+library(reactable)
 
 
 
 ui <- fluidPage(
+  titlePanel("COVID-19 retrospective publication analyis"),
   sidebarLayout(
-    sidebarPanel(selectInput("country", "Select country", multiple = TRUE, unique(readyData_for_correlation_covid$Country)))
+    sidebarPanel(selectInput("country", "Select country", unique(readyData_for_correlation_covid$Country)))
   ,
-  mainPanel(plotOutput("country")))
+  mainPanel(tabsetPanel(tabPanel("Plot",
+    plotOutput("country")),tabPanel("Table",reactableOutput("table")))))
   
   
   
@@ -20,7 +23,7 @@ server <- function(input, output) {
  zz <- reactive({readyData_for_correlation_covid %>% filter(Country == input$country)})
   
   output$country <- renderPlot({ggplot(zz(),aes(x=Total_cases_perMln,y=Articles_perMln)) + geom_point() + geom_smooth()})
-  
+  output$table <- renderReactable({reactable(zz())})
   
 }
 shinyApp(ui= ui, server = server)
